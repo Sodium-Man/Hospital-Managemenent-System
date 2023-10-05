bf9bd7d03e03afabbf7376a4a942fe12bd56b470
NamaN Bhalani <110275891+Sodium-Man@users.noreply.github.com>
2023-10-05 11:04:29 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -2,8 +2,8 @@ public class SumOfNumbers1
 {  
 public static void main(String args[])   
 {  
-int n1 = 225, n2 = 115, sum;  
+int n1 = 34, n2 = 35, sum;  
 sum = n1 + n2;  
-System.out.println("The sum of numbers is: "+sum);  
+System.out.println("The sum of two-numbers is: "+sum);  
 }  
 }  
